ab46a7c29b0ac8f4c0943a78e3706166a502193b
dsrasband@gmail.com <dsrasband@gmail.com>
2024-01-31 09:00:58 -0700

    entered direction comments

--- supporting.cs
+++ supporting.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// DAVIS AND MICHAEL'S DOMAIN!!
 namespace Mission4_team0201
 {
+    /*  - Receive the “board” array from the driver class
+        - Contain a method that prints the board based on the information passed to it
+        - Contain a method that receives the game board array as input and returns if there is a
+            winner and who it was */
     internal class supporting
     {
     }
