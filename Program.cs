395333aa0820be8f30242f0b3db8edda4c86c59a
harrisonstone <harrisonstone@HARRISONSTO949F>
2024-01-31 09:29:52 -0700

    driver

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Driver()
+{
+    public void Main()
+    {
+        Console.WriteLine("Welcome to Tic Tac Toe!");
+
+        char currentPlayer = "X";
+
+        Console.Write("Select row");
+        int row = int.Parse(Console.ReadLine());
+
+        if (int.Parse(row, row <= 3 && row >= 1)
+        {
+            Console.Write("Select column");
+            int col = int.Parse(Console.ReadLine());
+
+            if (int.Parse(col, col <= 3 && col >= 1)
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("Please select a valid column");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please select a valid row");
+        }
+
+
+        char[,] gameboard = new char[row, col];
+    }
+}
