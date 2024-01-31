71158015af47575d2df268655d65b2eafeb28a8f
dsrasband@gmail.com <dsrasband@gmail.com>
2024-01-31 09:06:15 -0700

    changed secondary class name to "Supporting"

--- supporting.cs
+++ supporting.cs
@@ -13,7 +13,8 @@ namespace Mission4_team0201
         - Contain a method that prints the board based on the information passed to it
         - Contain a method that receives the game board array as input and returns if there is a
             winner and who it was */
-    internal class supporting
+    internal class Supporting
     {
+
     }
 }
