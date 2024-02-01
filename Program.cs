1652c829253249a558c7814050a2427dbd6cbd4d
jonahallen <jonahmarkallen@gmail.com>
2024-01-31 17:57:10 -0700

    update

--- Program.cs
+++ Program.cs
@@ -1,34 +1,57 @@
-﻿class Driver()
+﻿//using supporting.cs;
+
+class Driver()
 {
-    public void Main()
+
+    static char currentPlayer = 'X';
+    static char[,] gameboard = new char[3, 3];
+    public static void Main()
     {
         Console.WriteLine("Welcome to Tic Tac Toe!");
 
-        char currentPlayer = "X";
+        Driver.createBoard();
+        //checkWinner();
+        Driver.playerMove();
 
-        Console.Write("Select row");
-        int row = int.Parse(Console.ReadLine());
-
-        if (int.Parse(row, row <= 3 && row >= 1)
+    }
+    
+    public static void createBoard()
+    {
+        for (int row = 0; row < 3; row++)
         {
-            Console.Write("Select column");
-            int col = int.Parse(Console.ReadLine());
-
-            if (int.Parse(col, col <= 3 && col >= 1)
+            for(int col = 0; col < 3; col++)
             {
-                
+                gameboard[row, col] = ' ';
+            }
+        }
+    }
+
+    public static void playerMove()
+    {
+
+        Console.Write("Player " + currentPlayer + " select row: ");
+        int row = int.Parse(Console.ReadLine()) - 1;
+
+        if (row >= 0 && row <= 2)
+        {
+            Console.Write("Player " + currentPlayer + " select column: ");
+            int col = int.Parse(Console.ReadLine()) - 1;
+
+            if ((col >= 0 && col <= 2) && gameboard[row,col] == ' ')
+            {
+                gameboard[row, col] = currentPlayer;
             }
             else
             {
                 Console.WriteLine("Please select a valid column");
+                playerMove();
             }
         }
         else
         {
             Console.WriteLine("Please select a valid row");
+            playerMove();
         }
-
-
-        char[,] gameboard = new char[row, col];
+     //Print board after each input and repeat through the playerMove method after alternating currentPlayer to "O";   
     }
 }
